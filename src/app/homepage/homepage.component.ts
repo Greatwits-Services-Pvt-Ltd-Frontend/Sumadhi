c98bc7644085ed90c22c02b0080d9f14e16c0210
sumit <sumit.sharma@greatwits.com>
2021-04-06 17:53:36 +0530

    6-4-21

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -13,9 +13,10 @@ export class HomepageComponent implements OnInit {
   constructor() { }
 
   
-  images = [701, 800, 807].map((n) => `https://picsum.photos/id/${n}/1400/508`);
+  images = [201, 20, 30].map((n) => `https://picsum.photos/id/${n}/1400/508`);
   ngOnInit(): void {
   }
+// ---------------------------------------
 
 }
 
